145f404b0ac198fda2af625996d77628aa0ebd24
davidrodriguez <davidrg131092@gmail.com>
2021-12-23 18:53:13 +0100

    brownie init and copying contract from previous repo

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
+
+contract SimpleStorage {
+    struct Person {
+        uint256 favouriteNumber;
+        string name;
+    }
+    Person[] public people;
+
+    mapping(string => uint256) public nameToFav;
+
+    uint256 public favouriteNumber;
+
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;
+    }
+
+    function get() public view returns (uint256) {
+        return favouriteNumber;
+    }
+
+    function compute(uint256 number) public pure {
+        number + number;
+    }
+
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        people.push(Person({favouriteNumber: _favouriteNumber, name: _name}));
+        nameToFav[_name] = _favouriteNumber;
+    }
+}
---
+++ installDeps.sh
@@ -0,0 +1,7 @@
+#python3 -m pip install -r requirements.txt
+
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+
+#logout, and then
+#pipx install eth-brwnie
