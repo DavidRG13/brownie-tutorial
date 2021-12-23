cdea9c6c3efb0435dca4c92647251017d8de4e11
davidrodriguez <davidrg131092@gmail.com>
2021-12-23 19:34:14 +0100

    test updating the value stored

--- tests/test_simple_storage.py
+++ tests/test_simple_storage.py
@@ -10,3 +10,17 @@ def test_deploy():
 
     # Then
     assert starting_value == 0
+
+def test_updating_value():
+    # Given
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+    starting_value = simple_storage.get()
+    value_to_update = starting_value + 15
+
+    # When
+    simple_storage.store(value_to_update, {"from": account})
+
+    # Then
+    updated_value = simple_storage.get()
+    assert updated_value == value_to_update
