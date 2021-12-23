a40098d9c60a07fd2d7dca2570632b32fb40821c
davidrodriguez <davidrg131092@gmail.com>
2021-12-23 19:45:18 +0100

    gets a real account to deploy on a test network

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,9 +1,15 @@
-from brownie import accounts, SimpleStorage
+from brownie import accounts, network, config, SimpleStorage
 
 def deploy_simple_storage():
-    account = accounts[0]
+    account = get_account()
     simple_storage = SimpleStorage.deploy({"from": account})
     print(simple_storage)
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 def main():
     deploy_simple_storage()
